b4234cac208e59b6e87c84986cbb468baecd8713
Amanbanti <amanbanti2011@gmail.com>
2025-02-13 17:53:33 +0300

    ninjasControllers commit

--- src/ninjas/ninjas.controller.ts
+++ src/ninjas/ninjas.controller.ts
@@ -1,4 +1,44 @@
-import { Controller } from '@nestjs/common';
-
+import { Controller,Get, Post,Delete,Put,Param,Query,Body } from '@nestjs/common';
+import { CreateNinjaDto} from './dto/create-ninja.dto';
+import {UpdateNinjaDto} from './dto/update-ninja.dto'
 @Controller('ninjas')
-export class NinjasController {}
+export class NinjasController {
+    //Get /ninjas?type=fast
+    @Get()
+    getNinjas(@Query('type') type: string){
+        return {type}
+    }
+
+    //Get /ninjas/:id
+    @Get(':id')
+    getOneNinja(@Param ('id')  id: string){
+        return {
+            id
+        }
+    }
+
+    //Post /ninjas
+    @Post()
+    createNinja(@Body() createNinjaDto:CreateNinjaDto){
+        return {
+            name:createNinjaDto.name
+        }
+    }
+
+    //Delete /ninjas/:id
+    @Delete(':id')
+    removeNinjas(@Param ('id') id: string){
+        return "deleting Ninjas at "+ id
+    }
+
+    @Put(':id')
+    updateNinja(@Param('id') id: string, @Body() updateNinjaDto: UpdateNinjaDto){
+        return {
+            id,
+            name:updateNinjaDto.name
+        }
+    }
+    
+
+}
+
